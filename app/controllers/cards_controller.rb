c4a1dea12f0be6e6ff7f2c1757d5c78bda4be269
tpayne84 <Travis.Payne84@me.com>
2013-09-27 20:07:49 -0500

    Implements card_params method to prevent strong paramaters from denying us from adding new words

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,10 +1,7 @@
 class CardsController < ApplicationController
-  def create
-    params.permit(:card, :title, :description, :deck_id)
-                             
-                             
+  def create                                            
     @deck = Deck.find(params[:deck_id])
-    @card = @deck.cards.build(params[:card])
+    @card = @deck.cards.build(card_params)
     @card.save
     redirect_to @deck
   end
@@ -14,4 +11,9 @@ class CardsController < ApplicationController
     @card.destroy
     redirect_to @card.deck
   end
+
+  def card_params
+    params.require(:card).permit(:title, :description, :deck_id)
+  end
+  
 end
